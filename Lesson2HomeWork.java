a2ea83eec4d10583eaff8bba40b697aa3a805c64
Geist-Elza <geist.elza@yandex.ru>
2022-03-31 18:32:27 +0700

    homework 2, 31.03.2022

---
+++ Lesson2HomeWork.java
@@ -0,0 +1,51 @@
+/**
+ * Java 1. Homework 2
+ * 
+ * @autor Tatyana Senchenko
+ * @version 31.03.2022
+ */
+ class Lesson2HomeWork {
+
+    public static void main(String[] args) {
+        System.out.println(isBetween10And20(10, -2));
+        System.out.println(isBetween10And20(12, 5));
+        System.out.println(isBetween10And20(20, 1));
+
+        printPositiveOrNegative(-1);
+        printPositiveOrNegative(0);
+        printPositiveOrNegative(1);
+
+        System.out.println(isNegative(-1));
+        System.out.println(isNegative(0));
+        System.out.println(isNegative(1));
+
+        restartString("Hello, Java!", 3);
+
+        System.out.println(isYearLeap(1904));
+        System.out.println(isYearLeap(1900));
+        System.out.println(isYearLeap(2000));
+        System.out.println(isYearLeap(2005));
+    }
+
+    static boolean isBetween10And20(int a, int b) {
+        return a + b >= 10 && a + b <= 20;
+    }
+
+    static void printPositiveOrNegative(int a) {
+        System.out.println(a >= 0? "Positive" : "Nergative");
+    }
+
+    static boolean isNegative(int a) {
+        return a < 0;
+    }
+
+    static void restartString(String str, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(str);
+        }
+    }
+
+    static boolean isYearLeap(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+ }
